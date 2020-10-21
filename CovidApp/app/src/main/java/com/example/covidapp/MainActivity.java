c5b787d3ba9e3576eb5f124e2a2fc20c78937347
Giorgos Lagoudakis <t8190087@aueb.gr>
2020-10-21 14:35:29 +0300

    First Project Upload

---
+++ CovidApp/app/src/main/java/com/example/covidapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerToAddButton();
+    }
+
+    private void addListenerToAddButton()
+    {
+        Button addButton = findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getNameFromEditText();
+            }
+        });
+    }
+
+    private void getNameFromEditText()
+    {
+        EditText nameEditText = findViewById(R.id.name_edit_text);
+        String name = nameEditText.getText().toString();
+        Log.i("NAME FROM EDIT TEXT", name);
+    }
+}
