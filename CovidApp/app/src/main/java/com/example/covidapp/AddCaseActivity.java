fb4b07a793104cf32daa704616ae05c01a5f9f8c
Giorgos Lagoudakis <t8190087@aueb.gr>
2020-11-02 15:29:59 +0200

    Added edit texts to Add Case Activity

--- CovidApp/app/src/main/java/com/example/covidapp/AddCaseActivity.java
+++ CovidApp/app/src/main/java/com/example/covidapp/AddCaseActivity.java
@@ -3,6 +3,11 @@ package com.example.covidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.example.covidapp.firebase.FirebaseFunctions;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AddCaseActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class AddCaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_case);
+    }
+
+
+    private void handleFirstName() {
+        try {
+            TextInputEditText firstNameEditText = findViewById(R.id.firstNameEditText);
+            String firstName = firstNameEditText.getText().toString();
+            if(TextUtils.isEmpty(firstName)) {
+                makeToast("First Name Is Empty");
+            }
+        } catch (NullPointerException e) {
+        }
+    }
+
+    private void makeToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
