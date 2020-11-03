6e0bfd5ab763e9e9c12401e2fa95a3b92d35bae3
Eleni Zoi <elenizoi96@gmail.com>
2020-11-04 00:07:04 +0200

    age, gender and months graphs

--- CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
+++ CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.example.covidapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +9,27 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.covidapp.R;
+import com.example.covidapp.stats.StatsInfoHolder;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +45,8 @@ public class StatisticsFragment extends Fragment {
         View fragmentView =  inflater.inflate(R.layout.fragment_statistics, container, false);
         setFragmentView(fragmentView);
         handleBarChart();
+        handlePieChart();
+        handleLineChart();
         return fragmentView;
     }
 
@@ -38,16 +57,97 @@ public class StatisticsFragment extends Fragment {
     private void handleBarChart() {
         BarChart topBarChart = fragmentView.findViewById(R.id.topBarChart);
         List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, StatsInfoHolder.getCases_january()));
+        entries.add(new BarEntry(1f, 20f));
         entries.add(new BarEntry(2f, 10f));
-        entries.add(new BarEntry(2f, 20f));
-        entries.add(new BarEntry(6f, 30f));
-        entries.add(new BarEntry(8f, 40f));
-        entries.add(new BarEntry(10f, 50f));
-        entries.add(new BarEntry(15f, 60f));
-        entries.add(new BarEntry(30f, 70f));
-        BarDataSet barDataSet = new BarDataSet(entries, "Cases By Month");
+        entries.add(new BarEntry(3f, 50f));
+        entries.add(new BarEntry(4f, 5f));
+        entries.add(new BarEntry(5f, 43f));
+        entries.add(new BarEntry(6f, 25f));
+        entries.add(new BarEntry(7f, 5f));
+        entries.add(new BarEntry(8f, 15f));
+        entries.add(new BarEntry(9f, 55f));
+        entries.add(new BarEntry(10f, 11f));
+        entries.add(new BarEntry(11f, 8f));
+        BarDataSet barDataSet = new BarDataSet(entries, "Months");
         BarData barData = new BarData(barDataSet);
         topBarChart.setData(barData);
         topBarChart.invalidate();
+        Description description = topBarChart.getDescription();
+        description.setText("");
+        barDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        //final String[] labels = new String[] {"Jan", "Feb", "March", "April", "May",
+                //"June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+        //XAxis xAxis = topBarChart.getXAxis();
+        //xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+        //xAxis.setGranularity(1f);
+        //xAxis.setGranularityEnabled(true);
+        //topBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+
     }
+
+    private void handlePieChart(){
+        PieChart secondPieChart = fragmentView.findViewById(R.id.secondPieChart);
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(60.5f, "Males"));
+        entries.add(new PieEntry(39.5f, "Females"));
+        PieDataSet set = new PieDataSet(entries, "");
+        PieData data = new PieData(set);
+        secondPieChart.setData(data);
+        secondPieChart.invalidate();
+        //set.setColor(new int[]{ R.color.colorPrimary}, Context);
+        //set2.setColor(new int[]{ R.color.material_on_surface_disabled}, Context);
+        set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        Description description = secondPieChart.getDescription();
+        description.setText("% confirmed cases based on gender");
+    }
+
+    private void handleLineChart(){
+        LineChart firstLineChart = fragmentView.findViewById(R.id.firstLineChart);
+        List<Entry> ageGroups = new ArrayList<Entry>();
+        Entry c1e1 = new Entry(0f, 20f);
+        ageGroups.add(c1e1);
+        Entry c1e2 = new Entry(1f, StatsInfoHolder.getCases_25_34());
+        ageGroups.add(c1e2);
+        Entry c1e3 = new Entry(2f, 50f);
+        ageGroups.add(c1e3);
+        Entry c1e4 = new Entry(3f, 35f);
+        ageGroups.add(c1e4);
+        Entry c1e5 = new Entry(4f, 22f);
+        ageGroups.add(c1e5);
+        Entry c1e6 = new Entry(5f, 37f);
+        ageGroups.add(c1e6);
+        Entry c1e7 = new Entry(6f, 13f);
+        ageGroups.add(c1e7);
+        Entry c1e8 = new Entry(7f, 40f);
+        ageGroups.add(c1e8);
+
+        LineDataSet setComp1 = new LineDataSet(ageGroups, "Ages groups");
+        setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        // use the interface ILineDataSet
+        List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+        dataSets.add(setComp1);
+
+        LineData data = new LineData(dataSets);
+        firstLineChart.setData(data);
+        firstLineChart.invalidate(); // refresh
+
+        // the labels that should be drawn on the XAxis
+        final String[] quarters = new String[] { "0-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "85+" };
+        ValueFormatter formatter = new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return quarters[(int) value];
+            }
+        };
+        XAxis xAxis = firstLineChart.getXAxis();
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+        Description description = firstLineChart.getDescription();
+        description.setText("");
+
+    }
+
 }
