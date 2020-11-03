c341ae3ebaee29520644d5dc8506d2dcbdee7079
Giorgos Lagoudakis <t8190087@aueb.gr>
2020-11-03 18:19:49 +0200

    Worked on the Statistics Fragment

--- CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
+++ CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
@@ -8,13 +8,46 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.covidapp.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatisticsFragment extends Fragment {
+
+    private View fragmentView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View fragmentView =  inflater.inflate(R.layout.fragment_statistics, container, false);
+        setFragmentView(fragmentView);
+        handleBarChart();
+        return fragmentView;
+    }
+
+    private void setFragmentView(View fragmentView) {
+        this.fragmentView = fragmentView;
+    }
+
+    private void handleBarChart() {
+        BarChart topBarChart = fragmentView.findViewById(R.id.topBarChart);
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(2f, 10f));
+        entries.add(new BarEntry(2f, 20f));
+        entries.add(new BarEntry(6f, 30f));
+        entries.add(new BarEntry(8f, 40f));
+        entries.add(new BarEntry(10f, 50f));
+        entries.add(new BarEntry(15f, 60f));
+        entries.add(new BarEntry(30f, 70f));
+        BarDataSet barDataSet = new BarDataSet(entries, "Cases By Month");
+        BarData barData = new BarData(barDataSet);
+        topBarChart.setData(barData);
+        topBarChart.invalidate();
     }
 }
