4f8d3a72ca6fd60a427c12abaf59a54419fee26b
elenatziveleki <t8190248@aueb.gr>
2020-11-15 23:33:34 +0200

    activity_view_contact

---
+++ CovidApp/app/src/main/java/com/example/covidapp/activity_view_contact.java
@@ -0,0 +1,14 @@
+package com.example.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_view_contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+    }
+}
