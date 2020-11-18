7685d94e22abdcb4794d2d742be6511ceb50ea4f
Giorgos Lagoudakis <t8190087@aueb.gr>
2020-11-18 13:36:04 +0200

    Worked on proper Layout Scaling and fixed bugs

--- CovidApp/app/src/main/java/com/example/covidapp/MainActivity.java
+++ CovidApp/app/src/main/java/com/example/covidapp/MainActivity.java
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.covidapp.fragments.CasesFragment;
 import com.example.covidapp.fragments.HomeFragment;
 import com.example.covidapp.fragments.StatisticsFragment;
 import com.google.android.material.tabs.TabLayout;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,16 +25,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.tab_layout);
-        setTabLayoutNavigationListener();
+        setUpTabLayout();
         setNewFragment(new HomeFragment(), "Home");
     }
 
     //Handles Tab Layout Clicks
-    private void setTabLayoutNavigationListener() {
+    private void setUpTabLayout() {
+
         tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_home_24));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_import_contacts_24));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_baseline_equalizer_24));
+
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(R.layout.main_activity_tab_layout);
+                TextView  textView = tab.getCustomView().findViewById(R.id.tabTitle);
+                switch (i) {
+                    case 0:
+                        textView.setText("Home");
+                        textView.setTextColor(getResources().getColor(R.color.topBarActiveText));
+                        break;
+                    case 1:
+                        textView.setText("Cases");
+                        textView.setTextColor(getResources().getColor(R.color.topBarInctiveText));
+                        break;
+                    case 2:
+                        textView.setText("Statistics");
+                        textView.setTextColor(getResources().getColor(R.color.topBarInctiveText));
+                }
+            }
+
+        }
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                TextView textView = tab.getCustomView().findViewById(R.id.tabTitle);
+                textView.setTextColor(getResources().getColor(R.color.topBarActiveText));
+                for(int i = 0; i < 3; i++) {
+                    if(i != tab.getPosition()) {
+                        TextView otherTextView = tabLayout.getTabAt(i).getCustomView().findViewById(R.id.tabTitle);
+                        otherTextView.setTextColor(getResources().getColor(R.color.topBarInctiveText));
+                    }
+                 }
                 switch (tab.getPosition()) {
                     case 0:
                         setNewFragment(new HomeFragment(), "Home");
