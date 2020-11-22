c5fab5e53b7bce30ac10423495f6606d9582e0b0
gsiandris <georgesiandris@gmail.com>
2020-11-22 23:48:04 +0200

    Minor Modifications

--- CovidApp/app/src/main/java/com/example/covidapp/fragments/HomeFragment.java
+++ CovidApp/app/src/main/java/com/example/covidapp/fragments/HomeFragment.java
@@ -32,9 +32,6 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         fragmentView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        TextView text = fragmentView.findViewById(R.id.simpleTextBottom);
-        text.setText("Total cases: "+ CasesFragment.returnCases());
         return fragmentView;
     }
 
