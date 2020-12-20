cdb8971969d3e6c236eeca590cddbe9732c4e04c
Eleni Zoi <elenizoi96@gmail.com>
2020-12-20 21:28:33 +0200

    Beautify StatisticsFragment.java

--- CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
+++ CovidApp/app/src/main/java/com/example/covidapp/fragments/StatisticsFragment.java
@@ -48,19 +48,23 @@ public class StatisticsFragment extends Fragment {
         // Inflate the layout for this fragment
         View fragmentView =  inflater.inflate(R.layout.fragment_statistics, container, false);
         setFragmentView(fragmentView);
-        //Calling the three methods for pie, line and bar chart
+        // Calling the three methods for pie, line and bar chart
         handlePieChart();
         handleLineChart();
         handleBarChart();
         return fragmentView;
     }
+
+    // Initializes the fragmentView field
     private void setFragmentView(View fragmentView) {
         this.fragmentView = fragmentView;
     }
 
     // Creating the Pie Chart
     private void handlePieChart(){
+        // Creating an object of the chart and connect it with the according one in the fragment_statistics.xml, using the id name
         PieChart secondPieChart = fragmentView.findViewById(R.id.secondPieChart);
+        // Creating a list with the entries of the Pie Chart
         List<PieEntry> entries = new ArrayList<>();
         // Taking the entries from the StatsInfoHolder, using the methods getCases_males and getCases_females
         entries.add(new PieEntry(StatsInfoHolder.getCases_males(), "Males"));
@@ -69,7 +73,7 @@ public class StatisticsFragment extends Fragment {
         PieDataSet set = new PieDataSet(entries, "");
         PieData data = new PieData(set);
         secondPieChart.setData(data);
-        // Refresh
+        // Refresh the pie chart
         secondPieChart.invalidate();
         // Setting colors using a template
         set.setColors(ColorTemplate.VORDIPLOM_COLORS);
@@ -79,13 +83,19 @@ public class StatisticsFragment extends Fragment {
         // Animation for the X and Y axis. 1,3 seconds graph's movement when the user goes to Statistics tab
         secondPieChart.animateXY(1300,1300);
     }
+
     // Creating the line chart
     private void handleLineChart(){
+        // Creating an object of the chart and connect it with the according one in the fragment_statistics.xml, using the id name
         LineChart firstLineChart = fragmentView.findViewById(R.id.firstLineChart);
         // Creating a list to put the entries for the graph from the StatsInfoHolder
         List<Entry> ageGroups = new ArrayList<Entry>();
+        // Creating the entries calling the appropriate methods
+        // The methods are separated according the age as you can see in the names
         Entry c1e1 = new Entry(0f, StatsInfoHolder.getCases_0_24());
+        // Once we create the entry, we add it to the list
         ageGroups.add(c1e1);
+        // Keep doing the same for every age group
         Entry c1e2 = new Entry(1f, StatsInfoHolder.getCases_25_34());
         ageGroups.add(c1e2);
         Entry c1e3 = new Entry(2f, StatsInfoHolder.getCases_35_44());
@@ -100,36 +110,41 @@ public class StatisticsFragment extends Fragment {
         ageGroups.add(c1e7);
         Entry c1e8 = new Entry(7f, StatsInfoHolder.getCases_85_plus());
         ageGroups.add(c1e8);
-
         // Setting the graph's label
         LineDataSet setComp1 = new LineDataSet(ageGroups, "Ages groups");
+        // We place the label on the left
         setComp1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        // Animation for the X and Y axis. 1,3 seconds graph's movement when the user goes to Statistics tab
         firstLineChart.animateXY(1300, 1300);
-
         // Use the interface ILineDataSet
         List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
         dataSets.add(setComp1);
-
         LineData data = new LineData(dataSets);
         firstLineChart.setData(data);
-        firstLineChart.invalidate(); // refresh
-
+        // Refresh the line chart
+        firstLineChart.invalidate();
         // The labels that should be drawn on the XAxis
         final String[] quarters = new String[] { "0-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75-84", "85+" };
+        // Method to format the quarters to have equal distance between them
         ValueFormatter formatter = new ValueFormatter() {
             @Override
             public String getAxisLabel(float value, AxisBase axis) {
                 return quarters[(int) value];
             }
         };
+        // Creating an object for the xAxis
         XAxis xAxis = firstLineChart.getXAxis();
-        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        // Minimum axis-step (interval) is 1
+        xAxis.setGranularity(1f);
+        // Using the object to call the method in order to format the distance between the quarters in xAxis
         xAxis.setValueFormatter(formatter);
-        Description description = firstLineChart.getDescription();
-        description.setText("");
+        // Making the description null because isn't useful for the specific chart
+        firstLineChart.setDescription(null);
     }
+
     // Creating the bar chart
     private void handleBarChart() {
+        // Creating an object of the chart and connect it with the according one in the fragment_statistics.xml, using the id name
         BarChart topBarChart = fragmentView.findViewById(R.id.topBarChart);
         // Creating a list to put the entries for the chart from the StatsInfoHolder
         List<BarEntry> entries = new ArrayList<>();
@@ -151,9 +166,9 @@ public class StatisticsFragment extends Fragment {
         BarData barData = new BarData(barDataSet);
         // Setting colors from a palette
         barDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-
-        // Creating a list for labels in x axis with the months' names
+        // Creating a list for labels in x axis
         final ArrayList<String> xAxisLabel = new ArrayList<>();
+        // Adding entries with the months' names in the list
         xAxisLabel.add("Jan");
         xAxisLabel.add("Feb");
         xAxisLabel.add("Mar");
@@ -166,14 +181,20 @@ public class StatisticsFragment extends Fragment {
         xAxisLabel.add("Oct");
         xAxisLabel.add("Nov");
         xAxisLabel.add("Dec");
-
+        // Creating an object of the xAxis
         XAxis xAxis = topBarChart.getXAxis();
+        // Using the object of the xAxis we created before in order to set the
+        // position of the labels on top of the specific axis
         xAxis.setPosition(XAxis.XAxisPosition.TOP);
         // Setting text size
         xAxis.setTextSize(10f);
+        // Drawing a line in the xAxis
         xAxis.setDrawAxisLine(true);
+        // Hiding the Gridlines from the axis
         xAxis.setDrawGridLines(false);
+        // Setting the color in black
         xAxis.setTextColor(Color.BLACK);
+        // Format the axis and the distance between the labels
         xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisLabel));
         xAxis.setGranularityEnabled(true);
         xAxis.setGranularity(1f);
@@ -183,11 +204,13 @@ public class StatisticsFragment extends Fragment {
         topBarChart.animateXY(1300, 1300);
         // Make the x-axis fit exactly all bars
         topBarChart.setFitBars(true);
+        // Make the description null
         topBarChart.setDescription(null);
+        // Make the labels in xAxis aligned in the center
         topBarChart.getXAxis().setCenterAxisLabels(true);
+        // Make the gridlines in the background to be invisible
         topBarChart.setDrawGridBackground(false);
-        // Refresh
+        // Refresh the bar chart
         topBarChart.invalidate();
     }
-
 }
