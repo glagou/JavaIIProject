6be6071e78732c076697182e9f0fc552a42e8d33
elenatziveleki <t8190248@aueb.gr>
2020-12-20 20:27:05 +0200

    comments addition

--- CovidApp/app/src/main/java/com/example/covidapp/activities/ViewContactActivity.java
+++ CovidApp/app/src/main/java/com/example/covidapp/activities/ViewContactActivity.java
@@ -15,7 +15,7 @@ public class ViewContactActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_contact);
         handleTextViews();
     }
-
+    
     private void handleTextViews() {
         TextView fullNameTextView = findViewById(R.id.fullNameText);
         TextView idTextView = findViewById(R.id.idText);
@@ -39,39 +39,39 @@ public class ViewContactActivity extends AppCompatActivity {
         closeContactWithTextView.setText(closeContactWith);
 
     }
-
+    //assign parameter to the variable fullName
     public static void setFullName(String fullName) {
         ViewContactActivity.fullName = fullName;
     }
-
+    //assign parameter to the variable id
     public static void setId(String id) {
         ViewContactActivity.id = id;
     }
-
+    //assign parameter to the variable phone
     public static void setPhone(String phone) {
         ViewContactActivity.phone = phone;
     }
-
+    //assign parameter to the variable residenceRegion
     public static void setResidenceRegion(String residenceRegion) {
         ViewContactActivity.residenceRegion = residenceRegion;
     }
-
+    //assign parameter to the variable dateOfDisease
     public static void setDateOfDisease(String dateOfDisease) {
         ViewContactActivity.dateOfDisease = dateOfDisease;
     }
-
+    //assign parameter to the variable gender
     public static void setGender(String gender) {
         ViewContactActivity.gender = gender;
     }
-
+    //assign parameter to the variable age
     public static void setAge(String age) {
         ViewContactActivity.age = age;
     }
-
+    //assign parameter to the variable susceptible
     public static void setSusceptible(String susceptible) {
         ViewContactActivity.susceptible = susceptible;
     }
-
+    //assign parameter to the variable closeContactWith
     public static void setCloseContactWith(String closeContactWith) {
         ViewContactActivity.closeContactWith = closeContactWith;
     }
